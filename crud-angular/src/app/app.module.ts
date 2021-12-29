dfa708eca9524e9a4f56f99104c1377cc0861974
oseias.junior <oseias.junior@pdcase.com.br>
2021-12-29 11:09:33 -0300

    nhame develop novamente

--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppTesteExcluirComponent } from './app-teste-excluir/app-teste-excluir.component';
 import { AppNhamenhame } from './app-nhamenhame.html/app-nhamenhame.html.component';
+import { AppNhamedevelopComponent } from './app-nhamedevelop/app-nhamedevelop.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppTesteExcluirComponent,
-    AppNhamenhame.HtmlComponent
+    AppNhamenhame.HtmlComponent,
+    AppNhamedevelopComponent
   ],
   imports: [
     BrowserModule,
