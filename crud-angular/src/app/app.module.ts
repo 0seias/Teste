aa3f57430f2a8053c520ab3b50f8c4a30c96015f
oseias.junior <oseias.junior@pdcase.com.br>
2021-12-29 10:39:29 -0300

    Mudança na develop

--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppTesteExcluirComponent } from './app-teste-excluir/app-teste-excluir.component';
+import { AppNhamenhame } from './app-nhamenhame.html/app-nhamenhame.html.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppTesteExcluirComponent,
+    AppNhamenhame.HtmlComponent
   ],
   imports: [
     BrowserModule,
