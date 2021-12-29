45a8f983693f1bad4f36b70eabe4c5e3c88cc549
oseias.junior <oseias.junior@pdcase.com.br>
2021-12-29 10:49:32 -0300

    master hmhm

---
+++ crud-angular/src/app/app-masternhame/app-masternhame.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-masternhame',
+  templateUrl: './app-masternhame.component.html',
+  styleUrls: ['./app-masternhame.component.scss']
+})
+export class AppMasternhameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
